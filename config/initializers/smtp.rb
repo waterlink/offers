572154d77def83db3fc7454121a7affb8a845955
Alexey Fedorov <waterlink000@gmail.com>
2014-03-29 18:09:01 +0200

    Fixed missing smtp problems

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,10 @@
 if Rails.env.staging? || Rails.env.production?
   SMTP_SETTINGS = {
-    address: ENV.fetch('SMTP_ADDRESS'), # example: 'smtp.sendgrid.net'
+    address: ENV['SMTP_ADDRESS'], # example: 'smtp.sendgrid.net'
     authentication: :plain,
-    domain: ENV.fetch('SMTP_DOMAIN'), # example: 'this-app.com'
-    password: ENV.fetch('SMTP_PASSWORD'),
+    domain: ENV['SMTP_DOMAIN'], # example: 'this-app.com'
+    password: ENV['SMTP_PASSWORD'],
     port: '587',
-    user_name: ENV.fetch('SMTP_USERNAME')
+    user_name: ENV['SMTP_USERNAME']
   }
 end
